4e695d07d3572d64cb37d809120ff7481c8191a4
flamenekof-web <flamenekof@gmail.com>
2026-06-05 07:03:58 +0500

    начанаем

---
+++ Quest.gd
@@ -0,0 +1,18 @@
+extends Resource
+class_name Quest
+
+@export var strenght: int 
+@export var harisma: int 
+@export var endurance: int 
+@export var intellect: int
+@export var agility: int 
+
+@export var name: String
+@export var description: String
+@export var reward: Variant
+
+@export var variant_1: String
+@export var variant_2: String
+@export var variant_3: String
+
+@export var slide: Texture2D
