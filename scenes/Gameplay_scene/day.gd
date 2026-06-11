785b443eb41047a3b2a67f3ef6f14a9898aa295b
flamenekof-web <flamenekof@gmail.com>
2026-06-11 16:58:46 +0500

    пенером

--- scenes/Gameplay_scene/day.gd
+++ scenes/Gameplay_scene/day.gd
@@ -6,7 +6,3 @@ extends Node
 func _ready() -> void:
 	quest_marker.open_window_quest.connect(quest_window.show_quest)
 	
-	var nav_region = $NavigationRegion2D
-	print("Cell Size: ", nav_region.cell_size)
-	print("Geometry Mask: ", nav_region.geometry_mask)
-	print("Parsed Geometry Type: ", nav_region.parsed_geometry_type)
